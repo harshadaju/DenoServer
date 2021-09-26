2500dee200f4cd284059103669c8c9ee23e43e57
Harshad Ahammed <harshadaju@gmail.com>
2021-09-26 14:35:29 +0530

    Second commit

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
-import { serve } from "https://deno.land/std@0.106.0/http/server.ts";
-const server = serve({ port: 8000 });
-console.log("http://localhost:8000/");
-for await (const req of server) {
-  req.respond({ body: "Hello World" });
-}
+addEventListener("fetch", (event) => {
+  const response = new Response("Hello World!", {
+    headers: { "content-type": "text/plain" },
+  });
+  event.respondWith(response);
+});
